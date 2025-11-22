1f5671d999c799932efa0f3d3a5919c30748b76d
Arshit rupani <125478383+arshitrupanii@users.noreply.github.com>
2025-11-22 20:51:51 +0530

    update

--- script.js
+++ script.js
@@ -10,45 +10,75 @@ let blocks = []
 
 let snake = [
     {
-        x: 1,
-        y: 5
-    },
-    {
-        x: 1,
-        y: 6
-    },
-    {
-        x: 1,
-        y: 7
-    },
-    {
-        x: 1,
+        x: 4,
         y: 8
-    }
+    },
+    // {
+    //     x: 1,
+    //     y: 9
+    // },
+    // {
+    //     x: 1,
+    //     y: 10
+    // }
 ]
 let direction = 'right';
 
+
+// this loop add block in board and blocks array
 for (let row = 0; row < rows; row++) {
     for (let col = 0; col < cols; col++) {
         const block = document.createElement("div");
         block.classList.add("block")
+        block.innerText = `${row}-${col}`
         board.appendChild(block);
-        blocks[` ${row}-${col} `] = block
+        blocks[`${row}-${col}`] = block
     }
 }
 
+// clear snake for per frame render
+function clearBoard() {
+    Object.values(blocks).forEach(b => b.classList.remove("fill"))
+}
+
+// render snake according to blocks
 function snakeRender() {
+    clearBoard()
+
     snake.forEach((segment) => {
-        const block = blocks[` ${segment.x}-${segment.y} `]
-        block.classList.add("fill")
+        const block = blocks[`${segment.x}-${segment.y}`]
+        if (block) { block.classList.add("fill") }
+        else {
+            console.log("game over")
+        }
     })
 }
 
-function RandomFood(){
 
-    blocks.forEach((block) => {
+setInterval(() => {
+    let head = null;
 
-    })
-}
+    if (direction == 'left') {
+        head = { x: snake[0].x, y: snake[0].y - 1 }
+    }
+    else if (direction == 'right') {
+        head = { x: snake[0].x, y: snake[0].y + 1 }
 
-snakeRender()
+    }
+    else if (direction == 'top') {
+        head = { x: snake[0].x - 1, y: snake[0].y }
+
+    }
+    else if (direction == 'down') {
+        head = { x: snake[0].x + 1, y: snake[0].y }
+    }
+
+    // it add head segment into array
+    snake.unshift(head)
+
+    // it remove last element of array
+    snake.pop()
+
+    snakeRender();
+
+}, 1000);
